3f43c018dbeba05beb5dbc4b0a2867a54f6a9e9b
Zai <lokky2082@gmail.com>
2018-06-05 00:46:32 +0500

    'images'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,12 +12,33 @@
                   </div>
                </div>
              </v-toolbar>
-              <v-card :color="'#ccccc'" height="67vh" width="100%" v-for="(item, key, i) in menu" v-if="item.active" :key="'slide' + i" >
+              <v-card :color="'#ccccc'" height="calc(100vh - 70px)" 
+              class="grid-card"
+               width="100%" 
+               v-for="(item, key, i) in menu" 
+               v-if="item.active" :key="'slide' + i" >
                 <v-card-title>
-                  <h2>{{item.name}}</h2>
+                  <h2 class="display-2 mb-5">{{item.name}}</h2>
                 </v-card-title>
-                 <v-card-text height="100%">
-                   
+                 <v-card-text>
+                   <div class="card-inner">
+                      <div class="card-list" v-if="lists">
+                            <v-card class="mb-4 list-card" row v-for="(item, i) in lists[key]" 
+                            :key="'list'+i"  @click.native="updateList(key, i)">
+                            <v-card-title class="display-1 card-ico-row">
+                                <span>
+                                  <v-icon v-if="item.icon" color="pink">star</v-icon>
+                                </span>
+                                  <span>{{item.title}}</span>
+                                </v-card-title>
+                            </v-card>
+                      </div>
+                      <div  v-if="imgs">
+                        <div class="card-img" v-for="(item, i) in imgs[key]" :key="'img'+i" v-if="item.seen">
+                            <img :src="item.img">
+                        </div>
+                      </div>
+                   </div>
                  </v-card-text>
                  <v-card-actions>
                     <v-spacer></v-spacer>
@@ -30,40 +51,6 @@
                     <v-spacer></v-spacer>
                   </v-card-actions>
               </v-card>
-           </v-layout>
-           <v-layout row wrap>
-                  <v-card>
-                      <v-card-title primary class="title">Тезисы</v-card-title>
-                      <v-card-text>
-                         <v-list dense>
-                            <v-list-tile>
-                              <v-list-tile-content>
-                                Почему нам нужно дробить клиентский код
-                              </v-list-tile-content>
-                            </v-list-tile>
-                            <v-list-tile>
-                              <v-list-tile-content>
-                                Какие системы у нас на поддержке
-                              </v-list-tile-content>
-                            </v-list-tile>
-                            <v-list-tile>
-                              <v-list-tile-content>
-                                Почему мы выбрали вебпак
-                              </v-list-tile-content>
-                            </v-list-tile>
-                            <v-list-tile>
-                              <v-list-tile-content>
-                               Система фронтенд разработки на сегодняшний день
-                              </v-list-tile-content>
-                            </v-list-tile>
-                            <v-list-tile>
-                              <v-list-tile-content>
-                                Система разработки фронтенда в будующем
-                              </v-list-tile-content>
-                            </v-list-tile>
-                         </v-list>
-                      </v-card-text>
-                   </v-card>
            </v-layout>
         </v-container>
   </div>
@@ -83,6 +70,8 @@ export default {
     return {
       curr: 0,
       menu: null,
+      lists: null,
+      imgs: null,
     };
   },
   computed: {
@@ -98,35 +87,64 @@ export default {
       const res = snapshot.val();
       this.menu = res;
     });
+    firebase.database().ref('lists').on('value', (snapshot) => {
+      const res = snapshot.val();
+      this.lists = res;
+    });
+    firebase.database().ref('imgs').on('value', (snapshot) => {
+      const res = snapshot.val();
+      this.imgs = res;
+    });
   },
   methods: {
     ...mapActions(['getSlides']),
+    updateList(key, i) {
+      console.log(key, i)
+      const n = i - 1;
+      const imageFalse = {
+        seen:false
+      };
+      const imageTrue = {
+        seen:true
+      };
+      const listItemTrue = {
+        icon: true
+      }
+      console.log('img',this.imgs[key][i]);
+      console.log('list', this.lists[key][i]);
+      var updates = {};
+      updates['/lists/' + key +'/'+ i] =  Object.assign(this.lists[key][i], listItemTrue);
+      if (i>0) {
+        updates['/imgs/' + key +'/'+ n] =  Object.assign(this.imgs[key][n], imageFalse);
+      }
+      updates['/imgs/' + key +'/'+ i] =  Object.assign(this.imgs[key][i], imageTrue);
+      firebase.database().ref().update(updates);
+    },
     updateSlides(key, way, i) {
       if (way === 'next') {
-        let next = Object.keys(this.menu)[i+1]
-        console.log(next)
+        const next = Object.keys(this.menu)[i + 1];
         const postDataDeactive = {
           active: false,
           seen: true,
-        }
+        };
         const postDataActive = {
           active: true,
           seen: false,
-        }
+        };
         const updates = {};
         updates[key] = Object.assign(this.menu[key], postDataDeactive);
         updates[next] = Object.assign(this.menu[next], postDataActive);
         firebase.database().ref('slider').update(updates);
       } else if (way === 'prev') {
-        let next = Object.keys(this.menu)[i-1]
+        const next = Object.keys(this.menu)[i - 1];
         const postDataDeactive = {
           active: false,
           seen: false,
-        }
+        };
         const postDataActive = {
           active: true,
           seen: false,
-        }
+        };
         const updates = {};
         updates[key] = Object.assign(this.menu[key], postDataDeactive);
         updates[next] = Object.assign(this.menu[next], postDataActive);
@@ -172,6 +190,42 @@ export default {
    }
    &.seen {
      color:#2196f3;
+   }
+ }
+ .card-inner {
+   display:grid;
+   grid-template-columns: 1fr 1fr;
+   grid-gap: 2%;
+ }
+ .grid-card {
+   display: grid;
+   grid-template-columns: 1fr;
+   grid-template-rows: 150px 1fr;
+   height: calc(100vh - 68px);
+ }
+ .card-ico-row {
+   display: grid;
+   grid-template-columns: 80px 1fr;
+   align-items: start;
+   text-align: left;
+ }
+ .card-list {
+   align-self: center;
+ }
+ .list-card {
+   cursor: pointer;
+   &:hover {
+     background: #cccccc;
+   }
+ }
+ .card-img {
+   position: relative;
+   img {
+    max-width: 80%;
+   margin-top: -5%;
+   display: block;
+   margin: -5% auto 0 auto;
+   max-height: 400px;
    }
  }
 </style>
